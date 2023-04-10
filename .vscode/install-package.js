28aea205c22660749f4582dc4694283f0c8f89f2
cweijan <cweijan@163.com>
2023-04-10 10:07:06 +0800

    Up script.

--- .vscode/install-package.js
+++ .vscode/install-package.js
@@ -1,9 +1,11 @@
 const cp = require('child_process');
+const { join } = require('path');
 
-const PACKAGED_FILE = './' + process.env.npm_package_name + '-' + process.env.npm_package_version + '.vsix';
-
-console.log('');
-cp.exec('code --install-extension ' + PACKAGED_FILE, { cwd: process.cwd() }, (err, stdout, stderr) => {
+const packagedFile = `./${process.env.npm_package_name}-${process.env.npm_package_version}.vsix`;
+const fullPath = join(process.cwd(), packagedFile)
+const cmd = `code --install-extension ${fullPath} --force`;
+console.log(`Executing: ${cmd}`);
+cp.exec(cmd, (err, stdout, stderr) => {
 	if (err) {
 		console.log('ERROR:');
 		console.log(err);
